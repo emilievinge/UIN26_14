12b01caafd1e2dedde2bba49e1fec16247c2a94f
emilievinge <emiliev@hiof.no>
2026-04-22 16:01:23 +0200

    Lagt til felles css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import './styles/layout.css'
 import Header from './compontents/Header'
 
 function App() {
