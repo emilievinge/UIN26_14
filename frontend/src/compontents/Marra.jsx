4928a3f60715a0aa851a60588d632aa279e8b2f4
Marra95w <maria.ulviksbakken@hiof.no>
2026-04-23 16:57:56 +0200

    Changes:
    Removed node modules & json files outside frontend og backend

--- frontend/src/compontents/Marra.jsx
+++ frontend/src/compontents/Marra.jsx
@@ -7,7 +7,7 @@ export default function Marra() {
       <img src="../Marra.png" alt="Generert bilde av Maria Ulviksbakken" id="marra-photo" />
       <h2 id="marra-name">Maria Ulviksbakken</h2>
       <p id="marra-mail">E-post: <a href="mailto:maria.ulviksbakken@hiof.no">maria.ulviksbakken@hiof.no</a></p>
-      <p>Studie: Bachelor Informasjonssystemer</p>
+      <p>Studie: Bachelor i Informasjonssystemer</p>
     </article>
   )
 }
