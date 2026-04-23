2d80b72ae9e8d02fd772522240e1d2fbd0daafb2
emilievinge <emiliev@hiof.no>
2026-04-23 16:50:42 +0200

    CSS farge tekst 💫

--- frontend/src/compontents/Vinge.jsx
+++ frontend/src/compontents/Vinge.jsx
@@ -6,8 +6,8 @@ export default function Vinge() {
         <article id='vinge-card'>
             <img src="../bilde_emilie.jpg" alt="Bilde av Emilie Vinge som barn" />
             <h2>Emilie Vinge</h2>
-            <a href="mailto:emilie.vinge@hiof.no">emilie.vinge@hiof.no</a>
-            <p>Bachelor i Informasjonssystemer</p>
+            <p>E-post: <a href="mailto:emilie.vinge@hiof.no">emilie.vinge@hiof.no</a></p>
+            <p>Studie: Bachelor i Informasjonssystemer</p>
         </article>
     )
 }
