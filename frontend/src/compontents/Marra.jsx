bb71720c328557599574cfb5ea4b7f86a76b8f0f
Marra95w <maria.ulviksbakken@hiof.no>
2026-04-23 16:52:17 +0200

    Changes:
    Changed id to marra

--- frontend/src/compontents/Marra.jsx
+++ frontend/src/compontents/Marra.jsx
@@ -3,10 +3,10 @@ import '../styles/marra.css'
 
 export default function Marra() {
   return (
-    <article id='maria-article'>
+    <article id='marra-article'>
       <img src="../Marra.png" alt="Generert bilde av Maria Ulviksbakken" id="marra-photo" />
-      <h2 id="maria-name">Maria Ulviksbakken</h2>
-      <p id="maria-mail">E-post: <a href="mailto:maria.ulviksbakken@hiof.no">maria.ulviksbakken@hiof.no</a></p>
+      <h2 id="marra-name">Maria Ulviksbakken</h2>
+      <p id="marra-mail">E-post: <a href="mailto:maria.ulviksbakken@hiof.no">maria.ulviksbakken@hiof.no</a></p>
       <p>Studie: Bachelor Informasjonssystemer</p>
     </article>
   )
