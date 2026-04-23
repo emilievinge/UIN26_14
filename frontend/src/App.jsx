1d3443ae3ffe8f14b20ea4a46ce782dd9b02fc03
Marra95w <maria.ulviksbakken@hiof.no>
2026-04-23 16:23:06 +0200

    Changes:
    Changed h2 to maria-name

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,7 @@ import './App.css'
 import './styles/layout.css'
 import Header from './compontents/Header'
 import Haugen from './compontents/Haugen'
+import Marra from './compontents/Marra'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -14,6 +15,7 @@ function App() {
     <>
       <Header />
       <Haugen />
+      <Marra />
     </>
   )
 }
--- frontend/src/compontents/Marra.jsx
+++ frontend/src/compontents/Marra.jsx
@@ -6,7 +6,7 @@ export default function Marra() {
     <article id='maria-article'>
       <img src="../Marra.png" alt="Generert bilde av Maria Ulviksbakken" id="marra-photo" />
       <h2 id="maria-name">Maria Ulviksbakken</h2>
-      <p id='maria-mail'>E-post: <a href="mailto:maria.ulviksbakken@hiof.no">maria.ulviksbakken@hiof.no</a></p>
+      <p id="maria-mail">E-post: <a href="mailto:maria.ulviksbakken@hiof.no">maria.ulviksbakken@hiof.no</a></p>
       <p>Studie: Bachelor Informasjonssystemer</p>
     </article>
   );
